a79ecd8bf0abf832078853b314fa0d70d05bb37d
taiga kume <taigak@taigas-MacBook-Pro.local>
2023-04-06 15:57:55 +0900

    initialized joinus view

---
+++ src/views/JoinusView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="about">
+  
+      <h1>履修希望の方へ</h1>
+      <p></p>
+  
+     
+  
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
