55a70fe55066bde85dc532781cea56b6aea58924
sakusaku3939 <yuukiyuuki223@gmail.com>
2024-04-13 05:44:47 +0900

    added a list of publications

---
+++ src/views/PublicationView.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="publications">
+
+    <h1>ジャーナル & 国際会議（査読有り）</h1>
+    <p>Bekku, Hiroo, et al. “Detecting Potholes from Dashboard Camera Images Using Ensemble of Classification
+      Mechanisms.” 2023 IEEE International Conference on Smart Computing (SMARTCOMP), 2023, pp. 108–15,
+      https://doi.org/10.1109/SMARTCOMP58114.2023.00031.</p>
+    <p>Yamada, Yusuke, et al. “C-SNE: Deep Cross-Modal Retrieval Based on Subjective Information Using Stochastic
+      Neighbor Embedding.” Journal of Information Processing (IPSJ), vol. 64, Apr. 2023, pp. 246–55,
+      https://doi.org/10.2197/ipsjjip.31.246.</p>
+    <p>川島寛乃, et al. “深層学習を用いた画像分類の継続学習におけるクラス難易度を考慮した容量制御.” 日本ソフトウェア科学会「コンピュータソフトウェア」,
+      2022.</p>
+    <p>Hamanaka, Satoki, et al. A Comparative Study of CIPN Symptom Estimation Methods Based on Machine Learning. 2021,
+      p. 4.</p>
+    <p>谷村朋樹, et al. “GANonymizer: 物体検出と敵対的生成を用いた映像匿名化手法.” 情報処理学会論文誌, vol. 60, no. 10,
+      2019, pp. 1829–44.
+      Okoshi, Tadashi, et al. “Real-World Large-Scale Study on Adaptive Notification Scheduling on Smartphones.”
+      Pervasive and Mobile Computing, vol. 50, Oct. 2018, pp. 1–24, https://doi.org/10.1016/J.PMCJ.2018.07.005.</p>
+
+    <h1>国内会議</h1>
+    <p>沙田佳穂, et al. “多クラス分類に向けた階層的分類モデル.” 研究報告ユビキタスコンピューティングシステム（UBI）, 2023, pp. 1–7.</p>
+    <p>福田峻, et al. 画像認識を利用した測定値の自動データ化. 2022.</p>
+    <p>川島寛乃, et al. “継続学習のクラス増分学習におけるクラス難易度を考慮したリプレイ手法の増加シナリオへの影響.” 研究報告ユビキタスコンピューティングシステム（UBI）,
+      2022, p. 2D5GS205--2D5GS205.</p>
+    <p>久米大雅, et al. “勾配法による蒸留における教師モデル選別.” 人工知能学会全国大会論文集 第 36 回 (2022), 2022, pp.
+      4Yin216--4Yin216.</p>
+    <p>坂内理人, et al. 物体検出を用いた道路陥没箇所検出モデルにおける合成画像を使用した学習の有効性の検討. 情報処理学会, 2021, p.
+      8.</p>
+    <p>別宮広朗, et al. 物体検出とセマンティックセグメンテーションによる車道上に限定した未知物体検出手法の提案. 情報処理学会, 2021, p. 8.</p>
+    <p>岩田あきほ, et al. “深層学習を用いたフィギュアスケートにおけるステップシークエンスのエレメント認識.” 2021年度人工知能学会全国大会（第35回）, 人工知能学会,
+      2021.</p>
+    <p>小澤遼, et al. “精度，推論速度及び消費電力の制御によるニューラルネットワークの自動構築化手法.” 研究報告ユビキタスコンピューティングシステム（UBI）,
+      vol. 2021-UBI69, 情報処理学会, 2021, pp. 1--8.</p>
+    <p>別宮広朗, et al. “ベイズ最適化と蒸留を用いた最適な圧縮モデル探索手法の提案.” 研究報告ユビキタスコンピューティングシステム（UBI）, vol.
+      2021-UBI69, 2021, pp. 1--8.</p>
+    <p>川島寛乃, et al. “償却推論にもとづいた継続学習.” 一般社団法人 人工知能学会 第34回全国大会(2020), 2020.</p>
+    <p>岩田あきほ, et al. “SkateNet:深層学習モデルを用いたフィギュアスケートにおけるステップシークエンスのエレメント認識.” 情報処理学会研究報告. UBI,
+      [ユビキタスコンピューティングシステム], vol. 2020-UBI-6, 2020.</p>
+    <p>川島寛乃, et al. “Neural Processによる意味的整合性を考慮した文書生成.” 第22回情報論的学習理論ワークショップ, 2019.</p>
+    <p>山田佑亮, et al. “CiCADA: 継続的なドメイン適応のための条件付き敵対的学習.” 第22回 画像認識・理解シンポジウム, 2019.</p>
+
+    <h1>その他業績</h1>
+    <a href="https://www.jn.sfc.keio.ac.jp/publication/">研究業績 | Nakazawa and Okoshi Lab.</a>
+  </div>
+</template>
+
+<style scoped>
+.publications {
+  min-height: 100vh;
+  align-items: center;
+  font-family: 'メイリオ', Meiryo, 'ＭＳ Ｐゴシック', 'Hiragino Kaku Gothic Pro',
+  'ヒラギノ角ゴ Pro W3', sans-serif;
+  padding: 0 3rem 3rem 3rem;
+}
+
+h1 {
+  margin-top: 3rem;
+  padding: 1rem;
+}
+
+p {
+  padding: 1rem;
+  line-break: anywhere;
+}
+
+a {
+  display: block;
+  padding: 0.5rem 1rem;
+}
+</style>
