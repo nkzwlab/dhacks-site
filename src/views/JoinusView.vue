7fc4028341af3c2063bc24343580fea51c7700e8
sakusaku3939 <yuukiyuuki223@gmail.com>
2025-08-13 06:52:13 +0900

    Rename to nakazawa and okoshi lab

--- src/views/JoinusView.vue
+++ src/views/JoinusView.vue
@@ -1,80 +1,79 @@
 <template>
-    <div class="joinus">
-  
-      <h1>履修希望の方へ</h1>
-      <p>履修希望の方は、以下の内容を確認の上、RG初回・第2回講義に来てください。質問のある方は <a href="mailto:d-hacks-group@keio.jp">d-hacks-group@keio.jp</a> までご連絡ください。</p>
+  <div class="joinus">
 
-      <h2>履修までの流れ </h2>
+    <h1>履修希望の方へ</h1>
+    <p>履修希望の方は、以下の内容を確認の上、RG初回・第2回講義に来てください。質問のある方は <a
+        href="mailto:d-hacks-group@keio.jp">d-hacks-group@keio.jp</a> までご連絡ください。</p>
 
-      <!-- <p> -->
-        <ol class="rounded-list">
-          <li><p>『履修要件』・『時間割』を確認する</p></li>
-          <li><p>RG全体の講義に参加する</p></li>
-          <li><p>d-hacksのミーティングを見学する</p></li>
-          <li><p>履修を確定させる</p></li>
-        </ol>
-      <!-- </p> -->
+    <h2>履修までの流れ </h2>
 
-      <h2>履修要件</h2>
-      <p>履修要件は原則として以下の通りとなります。「要件」とは言ってもあくまでも基準なので、迷っている方はぜひご相談ください。</p>
-      <p>
-        <ul>
-          <li>学年が1年生〜3年1学期目（3年2学期目の方は要相談）</li>
-          <li>情報基礎I・IIの成績がS・A以上であること（または充分にpythonが書けること）</li>
-          <li>高校で数学II・Bまではとっていること（または同等の知識を有していること）</li>
-          <li>研究会の講義と、d-hacksのミーティングに出席できること（既に授業を入れてしまっている場合はご相談ください。）</li>
-        </ul>
-      </p>
+    <ol class="rounded-list">
+      <li><p>『履修要件』・『時間割』を確認する</p></li>
+      <li><p>RG全体の講義に参加する</p></li>
+      <li><p>d-hacksのミーティングを見学する</p></li>
+      <li><p>履修を確定させる</p></li>
+    </ol>
 
-      <h2>時間割</h2>
-      <p>原則として以下の講義・ミーティングには出席していただく必要があります。出席が難しい場合は、ご相談ください。</p>
-      <p>
-        <ul>
-          <li>中澤研究室 ：木曜日2,3時限</li>
-          <li>d-hacks進捗報告会 ：木曜日4時限</li>
-          <li>d-hacks論文輪読会 ：木曜日5時限</li>
-        </ul>
-      </p>
+    <h2>履修要件</h2>
+    <p>
+      履修要件は原則として以下の通りとなります。「要件」とは言ってもあくまでも基準なので、迷っている方はぜひご相談ください。</p>
+    <ul>
+      <li>学年が1年生〜3年1学期目（3年2学期目の方は要相談）</li>
+      <li>情報基礎I・IIの成績がS・A以上であること（または充分にpythonが書けること）</li>
+      <li>高校で数学II・Bまでは取っていること（または同等の知識を有していること）</li>
+      <li>
+        中澤・大越研究室の全体講義と、d-hacksのミーティングに出席できること（既に授業を入れてしまっている場合はご相談ください。）
+      </li>
+    </ul>
 
-      <!-- <div class="faq">
-      <h2>よくある質問</h2>
-      </div>
-      <ul>
-        <li></li>
-      </ul> -->
+    <h2>時間割</h2>
+    <p>原則として以下の講義・ミーティングには出席していただく必要があります。出席が難しい場合は、ご相談ください。</p>
+    <ul>
+      <li>中澤・大越研 全体講義 ：木曜日2,3時限</li>
+      <li>d-hacks進捗報告会 ：木曜日4時限</li>
+      <li>d-hacks論文輪読会 ：木曜日5時限</li>
+    </ul>
+
+    <!-- <div class="faq">
+    <h2>よくある質問</h2>
     </div>
+    <ul>
+      <li></li>
+    </ul> -->
+  </div>
 
 
-  </template>
-  
-  <style scoped>
+</template>
+
+<style scoped>
 
 .joinus {
-    min-height: 100vh;
-    align-items: center;
-    font-family: 'メイリオ', Meiryo, 'ＭＳ Ｐゴシック', 'Hiragino Kaku Gothic Pro',
-      'ヒラギノ角ゴ Pro W3', sans-serif;
-    padding: 3rem;
-  }
-
-
-h1{
-  padding:1rem;
+  min-height: 100vh;
+  align-items: center;
+  font-family: 'メイリオ', Meiryo, 'ＭＳ Ｐゴシック', 'Hiragino Kaku Gothic Pro',
+  'ヒラギノ角ゴ Pro W3', sans-serif;
+  padding: 3rem;
 }
 
-h2{
-  padding:1rem;
+
+h1 {
+  padding: 1rem;
+}
+
+h2 {
+  padding: 1rem;
 }
 
 p {
-  padding:1rem;
+  padding: 1rem;
 }
 
-ol,ul {
-  width:100%;
-  padding-left:1rem;
+ol, ul {
+  width: 100%;
+  padding-left: 1rem;
 }
-/* 
+
+/*
 .rounded-list{
   counter-reset: li; 
   list-style: none; 
@@ -118,6 +117,6 @@ ol,ul {
   } */
 
 
-  </style>
+</style>
 <script setup lang="ts">
 </script>
